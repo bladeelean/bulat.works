18183419ffcd0c036332ced160e6cd064a3ef279
faes763 <amokrousova06@gmail.com>
2026-01-13 20:09:21 +0400

    feat: add new dialog

--- src/page/home/components/portfolio/mobile.tsx
+++ src/page/home/components/portfolio/mobile.tsx
@@ -11,6 +11,12 @@ export const PortfolioMobile = () => {
 
     const mobileData = [
         {
+            image: "/images/mobile/3.png",
+            name: <span>Инновационный сервис «Госконсьерж» <br/> Это новый способ получить Госуслуги путем видеозвонка</span>,
+            description: <span className=" ">Здесь я должен был сделать клиентский путь простым, а для операторов удобное рабочее пространство. Делал все: от концепта до передачи в разработку, но кейс-стади пока не сочинил</span>,
+            children: <DataThree />
+        },
+        {
             image: "/images/mobile/1.png",
             name: "Персональный анализ трат по сервисам",
             description: "Моей задачей было реализовать сценарий анализа трат, в данном кейсе, на транспорт, который позволяет пользователю отслеживать расходы по государственным и городским сервисам по месяцам, управлять категориями и видеть итоговую сумму",
@@ -22,12 +28,6 @@ export const PortfolioMobile = () => {
             description: "В продукте была задача повысить вовлечённость пользователей в выполнение целевых действий (авторизация, подтверждения, использование сервисов) без давления и прямой мотивации через уведомления",
             children: <DataTwo />
         }, 
-        {
-            image: "/images/mobile/3.png",
-            name: <span>Инновационный сервис «Госконсьерж» <br/> Это новый способ получить Госуслуги путем видеозвонка</span>,
-            description: <span className=" ">Здесь я должен был сделать клиентский путь простым, а для операторов удобное рабочее пространство. Делал все: от концепта до передачи в разработку, но кейс-стади пока не сочинил</span>,
-            children: <DataThree />
-        },
         {
             image: ["/images/mobile/4.png", "/images/mobile/5.png", "/images/mobile/6.png", "/images/mobile/7.png"],
             name: <span className=" text-gradient-welcome">Другие экраны...</span>
@@ -94,7 +94,7 @@ const DataOne = () => {
                         </DialogClose>
                     </DialogHeader>
                     <div className=" mt-14">
-                        <p className=" font-bold w-fit text-gradient-welcome text-5xl">Персональный анализ трат  ✦ по сервисам </p>
+                        <p className=" font-bold w-fit text-gradient-dialog-title text-5xl">Персональный анализ трат  ✦ по сервисам </p>
                         <p className=" text-2xl text-[#FFFFFF8F] mt-3 font-medium">Передо мной стояла задача реализовать сценарий анализа трат (в данном кейсе), на транспорт, который позволяет пользователю отслеживать расходы по государственным и городским сервисам по месяцам, управлять категориями и видеть итоговую сумму</p>
                     </div>
                     <div className=" mt-20">
@@ -102,13 +102,6 @@ const DataOne = () => {
                         <p className=" text-2xl text-[#FFFFFF66] mt-3 font-medium">Пользователи не могли быстро понять, из каких категорий и периодов формируются их расходы на транспорт. Данные существовали в виде отдельных операций, но не давали целостной картины и не позволяли сравнивать периоды и категории между собой. Обычная сводка формата View as list лишена эмоций и быстрого визуального сравнения</p>
                     </div>
                     <div className=" mt-10 bg-[#FFFFFF0A] py-16 rounded-[7.5rem] center gap-12">
-                        <Image
-                            className="  w-96 h-[52rem]"
-                            src={"/images/mobile/8.png"} 
-                            alt=""
-                            width={600}
-                            height={1000}
-                        />
                         <Image
                             className="  w-96 h-[52rem]"
                             src={"/images/mobile/9.png"} 
@@ -193,7 +186,7 @@ const DataTwo = () => {
                         </DialogClose>
                     </DialogHeader>
                     <div className=" mt-14">
-                        <p className=" font-bold w-fit text-gradient-welcome text-5xl">Геймификация с подарками ✦ от  МТС, Татнефти <br />и других партнеров</p>
+                        <p className=" font-bold w-fit text-gradient-dialog-title text-5xl">Геймификация с подарками ✦ от  МТС, Татнефти <br />и других партнеров</p>
                         <p className=" text-2xl text-[#FFFFFF8F] mt-3 font-medium">В продукте была задача повысить вовлечённость пользователей в выполнение целевых действий (авторизация, подтверждения, использование сервисов) без давления и прямой мотивации через уведомления</p>
                     </div>
                     <div className=" mt-20">
@@ -252,8 +245,183 @@ const DataTwo = () => {
 
 const DataThree = () => {
     return (
-        <div className="bg-[#FFFFFF14] h-14 w-fit mx-auto px-10 center text-2xl font-semibold rounded-full mt-8 cursor-pointer ">
-            Описания кейса пока нет
-        </div>
+        <Dialog>
+            <DialogTrigger className=" bg-[#FFFFFF14] h-14 w-28 center mx-auto rounded-full mt-8 cursor-pointer">
+                <Sprite
+                    name="arrow"
+                    className="size-8"
+                    pathSprite="icons/filled"
+                />
+            </DialogTrigger>
+            <DialogContent className=" overflow-auto p-0 flex flex-col sm:max-w-full inset-0  pb-24 translate-0 rounded-none border-none">
+                <div className=" container">
+                    <DialogHeader className=" sticky z-10 top-4 mt-32">
+                        <DialogClose className="flex cursor-pointer gap-4 items-center ">
+                            <div className=" bg-[#FFFFFF14] backdrop-blur h-14 w-20 center rounded-full cursor-pointer">
+                                <Sprite
+                                    name="arrow"
+                                    className="size-8 rotate-180"
+                                    pathSprite="icons/filled"
+                                />
+                            </div>
+                            <p className=" text-2xl font-semibold">Назад</p>
+                        </DialogClose>
+                    </DialogHeader>
+                    <div className=" mt-14">
+                        <p className=" font-bold w-fit text-gradient-dialog-title text-5xl">Инновационный сервис «Госконсьерж» <br /> Это новый способ получить Госуслуги звонком</p>
+                    </div>
+                    <div className=" mt-20">
+                        <p className=" font-bold w-fit text-4xl">Проблема пользователей и бизнес-цель</p>
+                        <div className=" mt-3 text-2xl font-medium text-[#FFFFFF8F]">
+                            <p>Для большинства пользователей получение государственных услуг ассоциируется со сложными формами, непонятными терминами и страхом допустить ошибку. Даже в цифровом формате процесс остаётся формальным и перегруженным.</p>
+                            
+                            <br/>
+
+                            <p className=" text-white">Пользователи:</p>
+                            <ul className=" list-disc pl-5">
+                                <li>не понимают, с чего начать,</li>
+                                <li>часто не доходят до завершения обращения.</li>
+                            </ul>
+                            
+                            <br/>
+                            
+                            <p className=" text-white">Бизнес-проблема:</p>
+                            <ul className=" list-disc pl-5">
+                                <li>часть обращений не доходила до результата,</li>
+                                <li>увеличивалась нагрузка на офлайн-каналы,</li>
+                                <li>снижалась удовлетворённость сервисом</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div className=" mt-10 h-[64rem] bg-[url(/images/mobile/16.png)] bg-cover rounded-[7.5rem]">
+
+                    </div>
+                    <div className=" mt-20">
+                        <p className=" font-bold w-fit text-4xl">Core Flow </p>
+                        <p className=" text-2xl text-[#FFFFFF66] mt-3 font-medium">Упрощенный сценарий сервиса, вокруг которого могут масштабироваться новые состояния, каналы и механики, не нарушая основную логику, т.к основные ветвления идут в процессе live-звонка – это различные всплывающие окна, подтверждения, тосты, алерты и прочее</p>
+                    </div>
+                    <div className=" mt-10 bg-[url(/images/mobile/17.png)] h-[77rem] bg-cover rounded-[7.5rem]">
+
+                    </div>
+                    <div className=" mt-20">
+                        <p className=" font-bold w-fit text-4xl">Решение </p>
+                        <div className=" mt-3 text-2xl font-medium text-[#FFFFFF8F]">
+                            <p>Я спроектировал сценарий, в котором пользователь:</p>
+
+                            <ul className=" list-disc pl-5">
+                                <li>сразу понимает, что сервис работает в формате видеозвонка,</li>
+                                <li>видит часы работы и доступность оператора,</li>
+                                <li>получает простое объяснение, что это за сервис и как он работает,</li>
+                                <li>может начать обращение одним действием.</li>
+                            </ul>
+                            <br/>
+                            <p>Интерфейс построен вокруг принципа: «Госуслуги – живой диалог с государством»</p>
+                        </div>
+                    </div>
+                    <div className=" mt-10 bg-[url(/images/mobile/18.png)] h-[66rem] bg-cover rounded-[7.5rem]">
+
+                    </div>
+                    <div className=" mt-20">
+                        <p className=" font-bold w-fit text-4xl">Проблемы после запуска</p>
+                        <p className=" text-2xl text-[#FFFFFF66] mt-3 font-medium">После запуска сервис столкнулся с высоким потоком обращений, который превысил наши ожидания — на старте не хватало операторов для обработки всех запросов. Метрики показали, что пользователи покидали сценарий на этапе длительного ожидания. Это стало отправной точкой для работы над задачей сохранения пользователя</p>
+                    </div>
+                    <div className=" mt-10 bg-black p-20 rounded-[7.5rem]">
+                        <p className=" font-bold w-fit text-5xl">Первые попытки решения <br /> выглядели вот так</p>
+                        <p className=" text-2xl text-[#FFFFFF66] mt-3 font-medium">Здесь я попробовал превратить ожидание в полезный этап: <br />предложить советы, помочь сформулировать вопрос, напомнить о документах, которые могут понадобиться на звонке. Но чтобы закрепить удержание, вторым шагом я добавил модальное окно — как дополнительную точку удержания. Оно появлялось в момент, когда пользователь уже готов был выйти из сценария, и предлагало альтернативу: не терять обращение, а получить уведомление, когда оператор освободится.</p>
+                        <div className=" h-[61rem] mt-11 bg-cover bg-[url(/images/mobile/19.png)]">
+                        </div>
+                    </div>
+                    <div className=" mt-20">
+                        <p className=" font-bold w-fit text-4xl">Результат попытки скромный</p>
+                        <p className=" text-2xl text-[#FFFFFF66] mt-3 font-medium">Снижение оттока на ~ 10%</p>
+                    </div>
+                    <div className=" mt-20 bg-black p-20 pb-16 rounded-[7.5rem]">
+                        <p className=" font-bold w-fit text-5xl">Попытка 2</p>
+                        <p className=" text-2xl text-[#FFFFFF66] mt-3 font-medium">Сразу после первой итерации я начал думать о следующем шаге. В этот момент у меня как раз пропал интернет в Chrome, и на экране появился тот самый офлайн-динозаврик. Тогда и возникла мысль: а почему бы не превратить ожидание оператора в похожий игровой сценарий. Параллельно я вспомнил про аналогичную фичу в Лавке. Пошел процесс...</p>
+                        <Image
+                            className=" mt-11 mx-auto w-96 h-[52rem]"
+                            src={"/images/mobile/21.png"} 
+                            alt=""
+                            width={600}
+                            height={1000}
+                        />
+                        <p className=" italic font-bold w-fit mt-11 text-4xl">Игра называется <span className=" ">«Бегущий Татарин»</span></p>
+                        <p className=" text-2xl text-[#FFFFFF66] mt-3 font-medium">Название пришло сразу. Поскольку этот релиз был ориентирован на Татарстан — один из самых цифровых регионов России, я решил встроить в визуал локальный контекст. В качестве образов использовал татарские мотивы и узнаваемые символы республики, чтобы игра выглядела живой и близкой пользователям.</p>
+                        <p className=" italic font-bold w-fit mt-11 text-4xl">Начальные спрайты для тестов механики</p>
+                        <div className=" mt-5 rounded-[3.5rem] bg-[#FFFFFF14] py-7 flex justify-center items-end gap-12">
+                            <Image
+                                src={"/images/mobile/22.png"} 
+                                alt=""
+                                width={256}
+                                height={400}
+                                className=" w-16 h-32"
+                            />
+                            <Image
+                                src={"/images/mobile/23.png"} 
+                                alt=""
+                                width={256}
+                                height={256}
+                                className=" w-16 h-16"
+                            />
+                            <Image
+                                src={"/images/mobile/24.png"} 
+                                alt=""
+                                width={256}
+                                height={296}
+                                className=" w-16 h-24"
+                            />
+                            <Image
+                                src={"/images/mobile/25.png"} 
+                                alt=""
+                                width={400}
+                                height={256}
+                                className=" w-32 h-12"
+                            />
+                            <Image
+                                src={"/images/mobile/26.png"} 
+                                alt=""
+                                width={256}
+                                height={256}
+                                className=" w-24 h-24"
+                            />
+                            <Image
+                                src={"/images/mobile/27.png"} 
+                                alt=""
+                                width={256}
+                                height={400}
+                                className=" w-16 h-32"
+                            />
+
+                        </div>
+                    </div>
+                    <div className=" mt-20">
+                        <p className=" font-bold w-fit text-4xl">Релиз и результаты</p>
+                        <div className=" mt-3 text-2xl font-medium text-[#FFFFFF8F]">
+                            <ul className=" list-disc pl-5">
+                                <li>Снизили отток на 38%</li>
+                                <li>Выявили поведенческие зависимости</li>
+                                <li>Немного повлияли на DAU / WAU, но выросло количество людей, которые просто зашли поиграть. Не сильно из-за этого расстроились т.к основную проблему получилось решить. Кстати, модалку мы также оставили и со временем добавили операторов, но это уже другие цифры</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div className=" mt-20">
+                        <p className=" font-bold w-fit text-4xl">Чем я занимался</p>
+                        <div className=" mt-3 text-2xl font-medium text-[#FFFFFF8F]">
+                            <ul className=" list-disc pl-5">
+                                <li>Определял продуктовую задачу и критерии успешности</li>
+                                <li>Исследовал поведение пользователей и бизнес-контекст</li>
+                                <li>Превращал инсайты в продуктовые гипотезы</li>
+                                <li>Выстраивал пользовательские сценарии и проверял их через прототипы</li>
+                                <li>Совместно с PM принимал решения о приоритетах и объёме релиза</li>
+                                <li>Доводил решения до production-качества</li>
+                                <li>Работал совместно с мобильщиками и QA</li>
+                                <li>Анализировал метрики после запуска</li>
+                                <li>Итеративное развитие продукта в течение нескольких кварталов после запуска</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </DialogContent>
+        </Dialog>
     );
 };

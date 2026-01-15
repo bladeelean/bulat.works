df45fa6bb2559984b35c27cf7d5168ac127303d7
bladeelean <docprofession2000@gmail.com>
2026-01-15 14:58:05 +0300

    Update button styles in portfolio component

--- src/page/home/components/portfolio/index.tsx
+++ src/page/home/components/portfolio/index.tsx
@@ -8,12 +8,12 @@ export const Portfolio = () => {
   href="/cv.pdf"
   target="_blank"
   rel="noopener noreferrer"
-  className="h-[62px] rounded-[20px]
-             inline-flex items-center justify-center
-             px-10
-             text-white text-2xl font-semibold leading-none
-             bg-gradient-to-r from-[#226DFF] to-[#3A93FF]
-             hover:brightness-110 transition"
+ className="h-[62px] rounded-[20px]
+           inline-flex items-center justify-center
+           px-10
+           text-white text-2xl font-semibold leading-none
+           bg-gradient-to-r from-[#8A81FB] via-[#1356FF] to-[#42A405]
+           hover:brightness-110 transition"
 >
   Посмотреть подробно в PDF
 </a>

f2a2bf9644875115a8424fd76434318694e3a234
bladeelean <docprofession2000@gmail.com>
2026-01-15 14:59:17 +0300

    Adjust button height and font weight
    Updated button height and font weight for styling consistency.

--- src/page/home/components/portfolio/index.tsx
+++ src/page/home/components/portfolio/index.tsx
@@ -8,10 +8,10 @@ export const Portfolio = () => {
   href="/cv.pdf"
   target="_blank"
   rel="noopener noreferrer"
- className="h-[62px] rounded-[20px]
+ className="h-[64px] rounded-[20px]
            inline-flex items-center justify-center
            px-10
-           text-white text-2xl font-semibold leading-none
+           text-white text-2xl font-medium leading-none
            bg-gradient-to-r from-[#8A81FB] via-[#1356FF] to-[#42A405]
            hover:brightness-110 transition"
 >

2a4574d5e74caf8c99482af261a98a978cd880dc
bladeelean <docprofession2000@gmail.com>
2026-01-15 14:49:58 +0300

    Update portfolio link styles in index.tsx

--- src/page/home/components/portfolio/index.tsx
+++ src/page/home/components/portfolio/index.tsx
@@ -8,8 +8,10 @@ export const Portfolio = () => {
           href="/cv.pdf"
           target="_blank"
           rel="noopener noreferrer"
-          className="inline-flex items-center justify-center rounded-2xl px-6 py-3 text-xl font-semibold
-                     bg-white/10 hover:bg-white/15
+          className="w-[249px] h-[62px] rounded-[20px] bg-[#226DFF]
+           flex items-center justify-center
+           text-white text-lg font-semibold
+           hover:opacity-90 transition"
         >
           Посмотреть подробно в PDF
         </a>

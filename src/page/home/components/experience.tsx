4750995cfe4b8e692dd2888182b79ee93fec5a50
bladeelean <docprofession2000@gmail.com>
2026-01-15 12:14:51 +0300

    Update experience.tsx

--- src/page/home/components/experience.tsx
+++ src/page/home/components/experience.tsx
@@ -1,25 +1,14 @@
 const experienceData = [
     {
-        name: "минцифры",
-        post: "Продуктовый дизайнер",
-        years: "2023 – ",
+        name: "Минцифры",
+        post: "Product manager",
+        years: "2024 – ",
         now: true,
     },
     {
-        name: "эдо «практика»",
-        post: "Продуктовый дизайнер",
-        years: "2021 – 2023",
-    },
-    {
-        name: "барс груп",
-        post: "UX/UI-дизайнер корпоративного портала",
-        years: "2021 – 2021",
-    },
-    {
-        name: "vexel",
-        post: "UX/UI-дизайнер финансового проекта Vexel.com",
-        years: "2019 – 2021",
-    },
+        name: "Брио МРС»",
+        post: "Product manager",
+        years: "2023 - 2024",
 ]
 
 
